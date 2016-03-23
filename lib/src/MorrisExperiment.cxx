a88864ef53c11fe657aadff1235df0f78c69510c
Sofiane HADDAD <haddad@imacs.polytechnique.fr>
2016-03-23 17:39:49 +0100

    Start implementation of MorrisExperiment

--- lib/src/MorrisExperiment.cxx
+++ lib/src/MorrisExperiment.cxx
@@ -37,6 +37,13 @@ static Factory<MorrisExperiment> RegisteredFactory;
 /* Default constructor */
 MorrisExperiment::MorrisExperiment()
   : WeightedExperiment()
+  , interval_()
+  , experiment_()
+  , step_()
+  , orientationMatrix_()
+  , permutationMatrix_()
+  , directionMatrix_()
+  , N_()
 {
   // Nothing to do
 }
@@ -44,20 +51,56 @@ MorrisExperiment::MorrisExperiment()
 /** Constructor using a p-level grid  - Uniform(0,1)^d */
 MorrisExperiment::MorrisExperiment(const Indices & levels, const UnsignedInteger N)
   : WeightedExperiment()
+  , interval_(levels.getSize())
+  , experiment_()
+  , step_ (levels.getSize())
+  , orientationMatrix_(Matrix(levels.getSize() + 1, levels.getSize()))
+  , permutationMatrix_(levels.getSize())
+  , directionMatrix_(levels.getSize())
+  , N_(N)
 {
-  // Nothing to do
+  // Compute step
+  for (UnsignedInteger k = 0; k < levels.getSize(); ++k)
+  {
+    if (levels[k] <= 1)
+      throw InvalidArgumentException(HERE) << "Levels should be at least 2; levels[" << k << "]=" << levels[k];
+    step_[k] = 1.0 / (levels[k] - 1.0);
+  }
 }
 
 /** Constructor using a p-level grid and intervals*/
 MorrisExperiment::MorrisExperiment(const Indices & levels, const Interval & interval, const UnsignedInteger N)
   : WeightedExperiment()
+  , interval_(interval)
+  , experiment_()
+  , step_ (levels.getSize())
+  , orientationMatrix_(Matrix(levels.getSize() + 1, levels.getSize()))
+  , permutationMatrix_(levels.getSize())
+  , directionMatrix_(levels.getSize())
+  , N_(N)
 {
-  // Nothing to do
+  if (levels.getSize() != interval.getDimension())
+    throw InvalidArgumentException(HERE) << "Levels and interval should be of same size. Here, level's size=" << levels.getSize()
+                                         <<", interval's size=" << interval.getDimension();
+  // Compute step
+  for (UnsignedInteger k = 0; k < levels.getSize(); ++k)
+  {
+    if (levels[k] <= 1)
+      throw InvalidArgumentException(HERE) << "Levels should be at least 2; levels[" << k << "]=" << levels[k];
+    step_[k] = 1.0 / (levels[k] - 1.0);
+  }
 }
 
 /** Constructor using NumericalSample, which is supposed to be an LHS design - Uniform(0,1)^d*/
 MorrisExperiment::MorrisExperiment(const NumericalSample & lhsDesign, const UnsignedInteger N)
   : WeightedExperiment()
+  , interval_(lhsDesign.getDimension())
+  , experiment_(lhsDesign)
+  , step_(NumericalPoint(lhsDesign.getDimension(), 1.0 / lhsDesign.getSize()))
+  , orientationMatrix_(lhsDesign.getDimension() + 1, lhsDesign.getDimension())
+  , permutationMatrix_(lhsDesign.getDimension())
+  , directionMatrix_(lhsDesign.getDimension())
+  , N_(N)
 {
   // Nothing to do
 }
@@ -65,8 +108,18 @@ MorrisExperiment::MorrisExperiment(const NumericalSample & lhsDesign, const Unsi
 /** Constructor using NumericalSample, which is supposed to be an LHS design */
 MorrisExperiment::MorrisExperiment(const NumericalSample & lhsDesign, const Interval & interval, const UnsignedInteger N)
   : WeightedExperiment()
+  , interval_(interval)
+  , experiment_(lhsDesign)
+  , step_(NumericalPoint(lhsDesign.getDimension(), 1.0 / lhsDesign.getSize()))
+  , orientationMatrix_(lhsDesign.getDimension() + 1, lhsDesign.getDimension())
+  , permutationMatrix_(lhsDesign.getDimension())
+  , directionMatrix_(lhsDesign.getDimension())
+  , N_(N)
+
 {
-  // Nothing to do
+  if (lhsDesign.getDimension() != interval.getDimension())
+    throw InvalidArgumentException(HERE) << "Levels and design should have same dimension. Here, design's dimension=" << lhsDesign.getDimension()
+                                         <<", interval's size=" << interval.getDimension();
 }
 
 /* Virtual constructor method */
@@ -98,7 +151,7 @@ void MorrisExperiment::save(Advocate & adv) const
   adv.saveAttribute( "step_", step_ );
   adv.saveAttribute( "orientationMatrix_", orientationMatrix_ );
   adv.saveAttribute( "permutationMatrix_", permutationMatrix_ );
-  adv.saveAttribute( "directionMatrix_", directionMatrix_ );
+  adv.saveAttribute( "directionMatrix_", directionMatrix_);
   adv.saveAttribute( "N_", N_ );
 }
 
