59b65bd8216954e814db6e185803804c1057e7af
Sofiane HADDAD <haddad@imacs.polytechnique.fr>
2016-04-12 16:01:39 +0200

    Update Morris function

--- python/test/morris_fun.py
+++ python/test/morris_fun.py
@@ -1,40 +1,70 @@
+import openturns as ot
 import numpy as np
 
-b0 = np.random.randn()
-b1 = np.concatenate((np.repeat(20,10), np.random.randn(10)))
+class MorrisFunction(ot.OpenTURNSPythonFunction):
+    """
+    The non-monotonic function of Morris f: R^20 -> R
 
-"""
-b2 <- rbind(cbind(matrix(-15, 6, 6),
-                  matrix(rnorm(6 * 14), 6, 14)),
-            matrix(rnorm(14 * 20), 14, 20))
-"""
-b3 = np.ndarray((20,)*3)
-b3[0 : 5, 0 : 5, 0 : 5] = - 10
-b4 = np.ndarray((20,)*4)
-b4[0 : 4, 0 : 4, 0 : 4, 0 : 4] = 5
+    Reference:
+      M. D. Morris, 1991, Factorial sampling plans for preliminary
+      computational experiments,Technometrics, 33, 161--174.
 
-def morris.function(x, b0, b1, b2):
-  # X is a list, transform it into array
-  X = np.array(x)
-  w = 2.0 * (X - 0.5)
-  w[:, [2, 4, 6] <- 2 * (1.1 * X[:, [2, 4, 6]] / (X[:, [2, 4, 6]] + .1) - 0.5)
-  y = b0
-  for i in range(20):
-      y += b1[i] * w[:, i]
+    """
+    def __init__(self, alpha, beta, gamma, b0):
+        ot.OpenTURNSPythonFunction.__init__(self, 20, 1)
+        self.b0 = float(b0)
+        # Check alpha dimension
+        assert(len(np.array(alpha).reshape(-1)) == 10)
+        self.b1 = np.concatenate((np.repeat(20,10), np.array(alpha).reshape(-1,)))
+        # Check beta and gamma dimension
+        assert(len(np.array(beta).reshape(-1)) == 6 * 14)
+        assert(len(np.array(gamma).reshape(-1)) == 20 * 14)
+        self.b2 = np.concatenate( (np.concatenate( (np.array([[-15]*6]*6), np.array(beta).reshape(6,14)), axis=1),
+                                   np.array(gamma).reshape(14,20)))
+        self.b3 = np.ndarray((20,)*3)
+        self.b3.fill(0)
+        self.b3[0 : 5, 0 : 5, 0 : 5] = - 10
+        self.b4 = np.ndarray((20,) * 4)
+        self.b4.fill(0)
+        self.b4[0 : 4, 0 : 4, 0 : 4, 0 : 4] = 5
 
-  for i in range(19):
-      for j in range(i + 1, 20):
-          y +=  b2[i, j] * w[:, i] * w[:, j]
+    def _exec(self, x):
+        assert (len(x)==20)
+        b1 = self.b1
+        b2 = self.b2
+        b3 = self.b3
+        b4 = self.b4
+        # X is a list, transform it into array
+        X = np.array(x)
+        w = 2.0 * (X - 0.5)
+        w = w.reshape(20,)
+        w[[2, 4, 6]] = 2.0 * (1.1 * X[[2, 4, 6]] / (X[[2, 4, 6]] + .1) - 0.5)
+        y = 0.0
+        y = np.dot(b1 , w)
+        # Morris function
+        for i in range(19):
+            for j in range(i + 1, 20):
+                y +=  b2[i, j] * w[i] * w[j]
+        for i in range(18):
+            for j in range(i + 1, 19):
+                for k in range(j + 1, 20):
+                    y += b3[i, j, k] * w[i] * w[j] * w[k]
 
-  for i in range(18):
-      for j in range(i + 1, 19):
-          for k in range(j + 1, 20):
-              y += y + b3[i, j, k] * w[:, i] * w[:, j] * w[:, k]
+        for i in range(17):
+            for j in range(i + 1, 18):
+                for k in range(j + 1, 20):
+                    for l in range(k + 1, 20):
+                        y += b4[i, j, k, l] * w[i] * w[j] * w[k] * w[l]
 
-  for i in range(17):
-      for j in range(i + 1, 18):
-          for k in range(j + 1, 20):
-              for l in range(k + 1, 20):
-                  y += b4[i, j, k, l] * w[:, i] * w[:, j] * w[:, k] * w[:, l]
+        return [y + self.b0]
 
-  return [y]
+if __name__== "__main__":
+    # Random ot points
+    ot.RandomGenerator.SetSeed(123)
+    b0 = ot.DistFunc.rNormal()
+    alpha = ot.DistFunc.rNormal(10)
+    beta =  ot.DistFunc.rNormal(6*14)
+    gamma =  ot.DistFunc.rNormal(20*14)
+    f = ot.NumericalMathFunction( MorrisFunction(alpha, beta, gamma, b0) )
+    input_sample = ot.ComposedDistribution([ot.Uniform(0,1)] * 20).getSample(20)
+    output_sample = f(input_sample)
