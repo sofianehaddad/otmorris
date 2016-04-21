3beee4b089d68b35d3453aff4c8b31c55bbcf159
Sofiane HADDAD <haddad@imacs.polytechnique.fr>
2016-04-21 16:16:44 +0200

    Update Morris test

--- python/test/t_Morris_std.py
+++ python/test/t_Morris_std.py
@@ -3,27 +3,22 @@
 from __future__ import print_function
 import openturns as ot
 import otmorris
+from otmorris.functions import MorrisFunction
 
 # Define model
-model = ot.NumericalMathFunction(["x","y"], ["sin(2*_pi*x)+y"])
+ot.RandomGenerator.SetSeed(1)
+alpha = ot.DistFunc.rNormal(10)
+beta = ot.DistFunc.rNormal(84)
+gamma = ot.DistFunc.rNormal(280)
+b0 = ot.DistFunc.rNormal()
 
-# Use Case 1 : starting from an LHS or optimized space filling LHS
-# Initial LHS design
-x = ot.LHSExperiment(ot.ComposedDistribution(2*[ot.Uniform(0,1)]), 5).generate()
+model = MorrisFunction(alpha, beta, gamma, b0)
+
 # Number of trajectories
-r = 4
-
-# First generate designs
-morris_experiment = otmorris.MorrisExperiment(x, r, ot.Interval(2))
-
-# Generate sample of size (p+1)*r where p is input dimension
-X = morris_experiment.generate()
-# Evaluation of the model on the design: evaluation outside OT
-Y = model(X)
-
-# Use Case 2 : define a k-grid level (so delta = 1/(k-1))
+r = 5
+# Define a k-grid level (so delta = 1/(k-1))
 k = 5
-morris_experiment = otmorris.MorrisExperiment(k, r, ot.Interval(2))
+morris_experiment = otmorris.MorrisExperiment([k] * 20, r)
 X = morris_experiment.generate()
 # Evaluation of the model on the design: evaluation outside OT
 Y = model(X)
@@ -31,9 +26,16 @@ Y = model(X)
 # Evaluation of Morris effects
 morris = otmorris.Morris(X, Y)
 
+print("Morris use case")
+print("Number of trajectories = ", r)
+
 # Get mean/sigma effects
-mean_effects = morris.getMeanEffects()
-sigma_effects = morris.getStandardDeviationEffects()
-# Plot effects
-fig = otmorris.plot(morris)
+mean_effects = morris.getMeanElementaryEffects()
+mean_abs_effects = morris.getMeanAbsoluteElementaryEffects()
+sigma_effects = morris.getStandardDeviationElementaryEffects()
+
+print("Elementary effects")
+print("Mean = ", mean_effects)
+print("Abs mean = ", mean_abs_effects)
+print("Sigma = ", sigma_effects)
 
