c8bfb4e668028bc33bcb1dbe7b4461efa717aa31
Julien Schueller <schueller@phimeca.com>
2019-07-30 09:26:29 +0200

    Fix example script
    Closes #28

--- python/doc/examples/example.py
+++ python/doc/examples/example.py
@@ -6,12 +6,14 @@ r = 10
 # Define experiments in [0,1]^20
 # p-levels
 p = 5
-morris_experiment = otmorris.MorrisExperimentGrid([p] * 20, r)
+dim = 20
+morris_experiment = otmorris.MorrisExperimentGrid([p] * dim, r)
+bounds = ot.Interval(dim) # [0,1]^d
 X = morris_experiment.generate()
 f = ot.Function(otmorris.MorrisFunction())
 Y = f(X)
 # Evaluate Elementary effects (ee)
-ee = otmorris.Morris(X, Y)
+ee = otmorris.Morris(X, Y, bounds)
 # Compute mu/sigma
 mean = ee.getMeanAbsoluteElementaryEffects()
 sigma = ee.getStandardDeviationElementaryEffects()
